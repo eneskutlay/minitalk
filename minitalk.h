4fa7321dd4e9a32b4df3b1ce436368d03bb1f3e0
Enes Kutlay <contact@eneskutlay.dev>
2022-06-13 20:50:46 +0300

    utils edited

--- minitalk.h
+++ minitalk.h
@@ -6,8 +6,18 @@
 /*   By: ekutlay <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/13 20:35:07 by ekutlay           #+#    #+#             */
-/*   Updated: 2022/06/13 20:35:08 by ekutlay          ###   ########.fr       */
+/*   Updated: 2022/06/13 20:48:26 by ekutlay          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#ifndef MINITALK_H
+# define MINITALK_H
 
+# include <signal.h>
+# include <unistd.h>
+# include <stdlib.h>
 
+void	ft_putchar(char c);
+void	ft_putstr(char *str);
+void	ft_putnbr(int nb);
+
+#endif
--- utils.c
+++ utils.c
@@ -6,8 +6,44 @@
 /*   By: ekutlay <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/13 20:34:05 by ekutlay           #+#    #+#             */
-/*   Updated: 2022/06/13 20:34:07 by ekutlay          ###   ########.fr       */
+/*   Updated: 2022/06/13 20:43:17 by ekutlay          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "minitalk.h"
 
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		write(1, &str[i++], 1);
+	}
+}
+
+void	ft_putnbr(int nb)
+{
+	unsigned int	i;
+
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		i = -nb;
+	}
+	else
+		i = nb;
+	if (i > 9)
+	{
+		ft_putnbr(i / 10);
+		ft_putchar(i % 10 + '0');
+	}
+	else
+		ft_putchar(i + '0');
+}
